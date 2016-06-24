76a05f29509ba6fd973c7367cbcdceda008a0d9d
Nicolas Sanchez <Nicolas Sanchez>
2016-06-24 10:52:11 -0300

    reloaded

--- src/main.c
+++ src/main.c
@@ -11,10 +11,26 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
-			
+#define BIT26 0x4000000
+#define BIT13 0x2000
+void delay (uint16_t delay);
 
-int main(void)
+void main(void)
 {
+	RCC->AHB1ENR|= RCC_AHB1ENR_GPIODEN;
+	GPIOD->MODER |= BIT26;
+	while (1) {
+		delay(5000);
+		GPIOD->ODR^=BIT13;
+	}
+}
+void delay(uint16_t delay)
+	{
+	uint8_t i;
+	while (delay-->0)
+	{
+		for (i=100;i>0;i--)
+		asm ("nop");
 
-	for(;;);
+	}
 }
